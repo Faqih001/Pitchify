2f9ea827e8f0bff0920c7aa26653618eb1ff5dec
Amirul Faqih <fakiiahmad001@gmail.com>
2025-01-12 22:39:36 +0300

    Add comment to clarify purpose of Layout component wrapping the app

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 
+// Layout component that wraps the entire app with Navbar and children
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <main className="font-work-sans">
