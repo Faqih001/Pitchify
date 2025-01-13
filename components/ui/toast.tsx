4d1bb06f6a37caec7288eedadadce5e348679aef
Amirul Faqih <fakiiahmad001@gmail.com>
2025-01-13 21:43:22 +0300

    Add comment to clarify the purpose of the ToastProvider component

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -7,6 +7,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
 
+// The ToastProvider component is a wrapper around the ToastPrimitives.Provider component
 const ToastProvider = ToastPrimitives.Provider;
 
 const ToastViewport = React.forwardRef<
