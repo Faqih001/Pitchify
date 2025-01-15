634072e9be962e895523d9ed8cff3fbbd7f41553
Amirul Faqih <fakiiahmad001@gmail.com>
2025-01-15 12:28:41 +0300

    Add comment to describe the StartupForm component

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -12,6 +12,7 @@ import { useToast } from "@/hooks/use-toast";
 import { useRouter } from "next/navigation";
 import { createPitch } from "@/lib/actions";
 
+// Startup form component that allows users to submit their startup pitch
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
   const [pitch, setPitch] = useState("");
