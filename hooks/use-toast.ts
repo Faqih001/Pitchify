a3577e08d30bf9b19ac26051a92f8f11a7a9a9aa
Amirul Faqih <fakiiahmad001@gmail.com>
2025-01-16 23:14:39 +0300

    Add comment to clarify the purpose of the memoryState variable in use-toast.ts

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -147,6 +147,7 @@ export const reducer = (state: State, action: Action): State => {
 // Array for the listeners
 const listeners: Array<(state: State) => void> = []
 
+// State for the toasts
 let memoryState: State = { toasts: [] }
 
 function dispatch(action: Action) {
