c30f5cbeeb228a5a7206f8da562dd2a523bf0db0
Amirul Faqih <fakiiahmad001@gmail.com>
2025-01-16 23:09:02 +0300

    Add comment to clarify purpose of UserStartups component

--- components/UserStartups.tsx
+++ components/UserStartups.tsx
@@ -3,6 +3,7 @@ import { client } from "@/sanity/lib/client";
 import { STARTUPS_BY_AUTHOR_QUERY } from "@/sanity/lib/queries";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
+// User startups component that displays the startups created by the user
 const UserStartups = async ({ id }: { id: string }) => {
   const startups = await client.fetch(STARTUPS_BY_AUTHOR_QUERY, { id });
 
