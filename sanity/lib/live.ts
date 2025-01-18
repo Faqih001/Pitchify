256b27a724c7d78f362defaa6bb5e85a765ad25c
Amirul Faqih <fakiiahmad001@gmail.com>
2025-01-18 20:23:10 +0300

    Add comment to clarify export of sanityFetch and SanityLive functions in live.ts

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -3,4 +3,5 @@ import "server-only";
 import { defineLive } from "next-sanity";
 import { client } from "@/sanity/lib/client";
 
+// Export the sanity fetch and sanity live functions with the client
 export const { sanityFetch, SanityLive } = defineLive({ client });
