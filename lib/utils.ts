0c955a971781b9b9bade97e1a9ab27b0b3dac123
Amirul Faqih <fakiiahmad001@gmail.com>
2025-01-18 20:21:42 +0300

    Add comment to clarify purpose of formatDate function in utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,6 +8,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+// function for formatting the date with the date string and returning the result
 export function formatDate(date: string) {
   return new Date(date).toLocaleDateString("en-US", {
     month: "long",
